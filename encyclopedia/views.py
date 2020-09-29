4d50e32dbf0ecdad7c6ac155b23d4329277962e2
Alexander Nikitin <snegopad1@gmail.com>
2020-09-30 00:56:07 +0300

    Add entry view

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponseNotFound
+
+from django.http import HttpResponse
 
 from . import util
 
@@ -8,3 +11,9 @@ def index(request):
         "entries": util.list_entries()
     })
 
+def entry(request, title):
+    e = util.get_entry(title)
+    if e:
+        return HttpResponse(e)
+    else:
+        return HttpResponseNotFound('<h1>Page not found</h1>')
